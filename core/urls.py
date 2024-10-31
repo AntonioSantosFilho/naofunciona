ce82f067e022f0ab6d7044a289fd21862ad7b8cb
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-10-31 20:58:20 -0300

    up

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,5 @@ from django.urls import path, include  # add this
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),                # Django admin route
-    path('accounts/', include('allauth.urls')),     # django all-auth
-    path("", include("apps.enquetes.urls")),             # UI Kits Html files  
+
 ]
