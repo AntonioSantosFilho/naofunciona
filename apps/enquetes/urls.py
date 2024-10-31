952401949beb8bf214492bdbcee7cc2614013e9e
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-10-31 20:57:48 -0300

    up

--- apps/enquetes/urls.py
+++ apps/enquetes/urls.py
@@ -1,13 +1,12 @@
+
+from django.contrib import admin
 from django.urls import path
 
 from . import views
 
 app_name = 'enquetes'
 
+
 urlpatterns = [
-	    
-    path('', views.listar, name='listar'),
-    path('<int:question_id>/', views.detalhar, name='detalhar'),
-    path('<int:question_id>/resultados/', views.resultados, name='resultados'),
-    path('<int:question_id>/votar/', views.votar, name='votar'),
+
 ]
