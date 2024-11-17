922dd9a54dea9933d81e727454c678023057fb90
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-11-17 16:52:13 -0300

    change app name

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ Copyright (c) 2019 - present AppSeed.us
 from django.contrib import admin
 from django.urls import path, include
 
-from apps.enquetes import views  # add this
+from apps.registro import views  # add this
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),                # Django admin route
